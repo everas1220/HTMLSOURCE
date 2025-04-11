76bf86807d6f612049a2fb4a5d7a52f4050f9c2d
shin seung woo <ssw030209@gmail.com>
2025-04-11 17:53:56 +0900

    인터렉션 구문 예제

---
+++ js/ajax/ajax.js
@@ -0,0 +1,15 @@
+// console.log("시작");
+// setTimeout(() => {
+//     console.log("중간");
+// }, 3000);
+// console.log("마무리");
+
+//  fetch
+// fetch("서버주소").then("응답도착").then("도착한응답처리")
+
+// console.log("시작");
+// ('https://jsonplaceholder.typicode.com/todos/1') 요청
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+// console.log("마무리");
--- js/regex/ajax/ajax.js
+++
@@ -1,5 +0,0 @@
-console.log("시작");
-setTimeout(() => {
-    console.log("중간");
-}, 3000);
-console.log("마무리");
